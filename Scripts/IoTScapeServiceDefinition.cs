5e1a3d78cf6acc0d3e6acd0e423fb746d823101c
Gordon Stein <gstein@ltu.edu>
2021-07-20 16:31:29 -0500

    Editor updates service definition version on edit

--- Scripts/IoTScapeServiceDefinition.cs
+++ Scripts/IoTScapeServiceDefinition.cs
@@ -11,5 +11,14 @@ namespace IoTScapeUnityPlugin
         public string id;
         public GenericDictionary<string, IoTScapeMethodDescription> methods = new GenericDictionary<string, IoTScapeMethodDescription>();
         public GenericDictionary<string, IoTScapeEventDescription> events = new GenericDictionary<string, IoTScapeEventDescription>();
+
+        public void OnValidate()
+        {
+            // Update version when changed
+            if(int.TryParse(service.version, out int versionNumber))
+            {
+                service.version = (versionNumber + 1).ToString();
+            } 
+        }
     }
 }
